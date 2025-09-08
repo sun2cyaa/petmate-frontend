4fc9b90054b81d5dec00708fb000f4a0399b5b6f
4424904 <4424904@gmail.com>
2025-09-08 14:20:06 +0900

    [20250908_08_KST] 세번째 섹션 작업

--- src/pages/common/Intro/FeedbackFlowSection.jsx
+++ src/pages/common/Intro/FeedbackFlowSection.jsx
@@ -118,16 +118,54 @@ function FeedbackFlowSection() {
   const [activeHearts, setActiveHearts] = useState({});
 
   const reviews = [
-    { id: 1, name: '김민중', pet: '골든 리트리버', text: '초코를 돌봐주셔서 다시 한번 감사합니다. 초코가 정말 즐거운 시간을 보냈어요.', profileImg: "https://picsum.photos/200?random=1" },
-    { id: 2, name: '독고건', pet: '말티즈', text: '우리집 강아지가 너무 좋아해서 또 방문할게요!', profileImg: "https://picsum.photos/200?random=2" },
-    { id: 3, name: '김형선', pet: '포메 + 치와와', text: '쿠키를 다시 한번 돌봐주셔서 감사합니다.', profileImg: "https://picsum.photos/200?random=3" },
-    { id: 4, name: '김광현', pet: '시츄', text: '형욱님 정말 최고였어요! 집과 마당이 정말 아름다웠고, 우리 강아지는 형욱님이랑 함께 시간보내는걸 정말 즐거워 하더라고요.', profileImg: "https://picsum.photos/200?random=4" },
-    { id: 5, name: '박선희', pet: '푸들', text: '돌봄 서비스가 너무 체계적이라 안심이 됐습니다. 덕분에 출장도 걱정 없이 다녀왔어요.', profileImg: "https://picsum.photos/200?random=5" },
-    { id: 6, name: '김성택', pet: '비숑프리제', text: '처음 맡겼는데 사진도 계속 보내주셔서 마음이 편했어요. 다음에도 이용할게요!', profileImg: "https://picsum.photos/200?random=6" },
-    { id: 7, name: '선태영', pet: '시바견', text: '산책도 충분히 해주시고, 강아지가 에너지를 잘 발산하고 와서 너무 만족합니다.', profileImg: "https://picsum.photos/200?random=7" },
-    { id: 8, name: '김도훈', pet: '믹스견', text: '사교성이 부족한 아이인데도 잘 케어해주셔서 점점 나아지는 것 같아요. 감사합니다!', profileImg: "https://picsum.photos/200?random=8" },
-    { id: 9, name: '김동현', pet: '웰시코기', text: '짧은 기간이었지만 너무 친절하게 돌봐주셔서 고마워요. 강아지도 집에 와서 계속 행복해 보이네요.', profileImg: "https://picsum.photos/200?random=9" },
-    { id: 10, name: '홍자영', pet: '스피츠', text: '예약부터 픽업까지 과정이 깔끔했어요. 서비스가 정말 믿음직스럽습니다.', profileImg: "https://picsum.photos/200?random=10" }
+    { id: 1, name: '김민중', 
+             pet: '골든 리트리버', 
+             text: '초코를 돌봐주셔서 다시 한번 감사합니다. 초코가 정말 즐거운 시간을 보냈어요.', 
+             profileImg: "https://picsum.photos/200?random=1" },
+
+    { id: 2, name: '독고건', 
+             pet: '말티즈', 
+             text: '우리집 강아지가 너무 좋아해서 또 방문할게요!', 
+             profileImg: "https://picsum.photos/200?random=2" },
+
+    { id: 3, name: '김형선', 
+             pet: '포메 + 치와와', 
+             text: '쿠키를 다시 한번 돌봐주셔서 감사합니다.', 
+             profileImg: "https://picsum.photos/200?random=3" },
+
+    { id: 4, name: '김광현', 
+             pet: '시츄', 
+             text: '형욱님 정말 최고였어요! 집과 마당이 정말 아름다웠고, 우리 강아지는 형욱님이랑 함께 시간보내는걸 정말 즐거워 하더라고요.', 
+             profileImg: "https://picsum.photos/200?random=4" },
+
+    { id: 5, name: '박선희', 
+             pet: '푸들', 
+             text: '돌봄 서비스가 너무 체계적이라 안심이 됐습니다. 덕분에 출장도 걱정 없이 다녀왔어요.', 
+             profileImg: "https://picsum.photos/200?random=5" },
+
+    { id: 6, name: '김성택', 
+             pet: '비숑프리제', 
+             text: '처음 맡겼는데 사진도 계속 보내주셔서 마음이 편했어요. 다음에도 이용할게요!', 
+             profileImg: "https://picsum.photos/200?random=6" },
+
+    { id: 7, name: '선태영', 
+             pet: '시바견', 
+             text: '산책도 충분히 해주시고, 강아지가 에너지를 잘 발산하고 와서 너무 만족합니다.', 
+             profileImg: "https://picsum.photos/200?random=7" },
+    { id: 8, name: '김도훈', 
+             pet: '믹스견', 
+             text: '사교성이 부족한 아이인데도 잘 케어해주셔서 점점 나아지는 것 같아요. 감사합니다!', 
+             profileImg: "https://picsum.photos/200?random=8" },
+
+    { id: 9, name: '김동현', 
+             pet: '웰시코기', 
+             text: '짧은 기간이었지만 너무 친절하게 돌봐주셔서 고마워요. 강아지도 집에 와서 계속 행복해 보이네요.', 
+             profileImg: "https://picsum.photos/200?random=9" },
+
+    { id: 10, name: '홍자영', 
+              pet: '스피츠', 
+              text: '예약부터 픽업까지 과정이 깔끔했어요. 서비스가 정말 믿음직스럽습니다.', 
+              profileImg: "https://picsum.photos/200?random=10" }
   ];
 
   const toggleHeart = (reviewId) => {
