6139c9d1d94a301ff11fb426400cccfb2ea294cb
4424904 <4424904@gmail.com>
2025-09-18 19:12:07 +0900

    [20250918_19_KST] 헤더 및 상품목록 수정

--- src/components/common/Header/Notice.jsx
+++ src/components/common/Header/Notice.jsx
@@ -8,11 +8,11 @@ const Notice = () => {
       <div className="notice-list">
         <div className="notice-item">
           <h3>[공지] PetMate 서비스 오픈 안내</h3>
-          <p>2025년 9월 1일부터 PetMate 서비스가 정식으로 오픈되었습니다!</p>
+          <p>2025년 9월 22일부터 PetMate 서비스가 정식으로 오픈되었습니다!</p>
         </div>
         <div className="notice-item">
           <h3>[점검] 9월 서버 점검 안내</h3>
-          <p>9월 20일 새벽 2시 ~ 4시까지 서버 점검이 예정되어 있습니다.</p>
+          <p>9월 23일 새벽 2시 ~ 4시까지 서버 점검이 예정되어 있습니다.</p>
         </div>
       </div>
     </div>
