226aca46e163a5f8c1a19e5f78dcb0548a91a36b
SkynDeX <passkery@gmail.com>
2025-09-20 00:16:32 +0900

    [20250920_01_Gun] 도커 서버 셋팅

--- src/pages/Auth/SigninPage.jsx
+++ src/pages/Auth/SigninPage.jsx
@@ -5,8 +5,8 @@ import googleIcon from "../../assets/icons/login/google.png";
 
 const API_BASE =
   (typeof import.meta !== "undefined" && import.meta.env?.VITE_API_BASE) ||
+  process.env.REACT_APP_API_BASE_URL ||
   process.env.REACT_APP_SPRING_API_BASE ||
-  process.env.REACT_APP_API_BASE ||
   "http://localhost:8090";
 
 export default function SigninPage() {
